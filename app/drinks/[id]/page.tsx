6d1f0c83ba98bba4970aa3bbfcecf715e22a7c27
Guilherme Estevan <guilhermeestevangui@hotmail.com>
2024-06-07 00:14:35 -0300

    images

--- app/drinks/[id]/page.tsx
+++ app/drinks/[id]/page.tsx
@@ -1,7 +1,37 @@
-const SingleDrinkPage = ({ params }: { params: { id: string } }) => {
-  console.log(params);
+import Link from "next/link";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+import Image from "next/image";
 
-  return <div>SingleDrinkPage</div>;
+const getSingleDrink = async (id: string) => {
+  const response = await fetch(url + id);
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  return data;
+};
+
+const SingleDrinkPage = async ({ params }: { params: { id: string } }) => {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
+
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        back to drinks
+      </Link>
+      <Image
+        src={imgSrc}
+        alt="drink"
+        width={300}
+        height={300}
+        className="w-48 h-48 rounded-lg
+        shadow-lg mb-4"
+      />
+      <h1 className="text-4xl mb-8">{title}</h1>
+    </div>
+  );
 };
 
 export default SingleDrinkPage;
--- components/DrinksList.tsx
+++ components/DrinksList.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const DrinksList = ({ drinks }: { drinks: any }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
       {drinks.map((drink: any) => {
         return (
           <li key={drink.id}>
@@ -10,6 +10,15 @@ const DrinksList = ({ drinks }: { drinks: any }) => {
               href={`/drinks/${drink.idDrink}`}
               className="text-xl font-medium"
             >
+              <div className="relative h-48 mb-4">
+                <Image
+                  src={drink.strDrinkThumb}
+                  alt={drink.strDrink}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="rounded-md object-cover"
+                />
+              </div>
               {drink.strDrink}
             </Link>
           </li>
