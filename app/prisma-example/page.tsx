16a3b22d5eadb43eedf8ec2275c63d6a872b874f
Guilherme Estevan <guilhermeestevangui@hotmail.com>
2024-06-10 22:37:21 -0300

    prisma

--- app/prisma-example/page.tsx
+++ app/prisma-example/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import prisma from "@/utils/db";
 
-const page = () => {
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
+};
+
+const PrismaExample = async () => {
+  const tasks = await prismaHandlers();
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1 className="text-7xl">Prisma Example</h1>
+      {tasks.map((task) => {
+        return <h2 key={task.id}>{task.content}</h2>;
+      })}
+    </div>
+  );
+};
 
-export default page
+export default PrismaExample;
